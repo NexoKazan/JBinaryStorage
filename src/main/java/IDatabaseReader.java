08d392aabf17c3bc5bd100e783abae280d846298
Игорь Казанцев <Rexitt@yandex.ru>
2022-09-13 16:46:45 +0300

    1

---
+++ src/main/java/IDatabaseReader.java
@@ -0,0 +1,9 @@
+import java.io.IOException;
+import java.util.List;
+
+public interface IDatabaseReader {
+    FileMeta GetMeta();
+    Object[] ReadNextRow() throws IOException, ClassNotFoundException;
+    List<byte[]> ReadNextRowRaw();
+    void close() throws Exception;
+}
