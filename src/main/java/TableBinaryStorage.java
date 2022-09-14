6c408202be389b11c5e1f15eb671b30077f44252
Игорь Казанцев <Rexitt@yandex.ru>
2022-09-14 13:12:48 +0300

    pre-CalciteIntegration. Can read files more than 2gb. Can create binary files.

--- src/main/java/TableBinaryStorage.java
+++ src/main/java/TableBinaryStorage.java
@@ -1,4 +1,5 @@
 import com.google.gson.Gson;
+import org.apache.commons.lang3.ArrayUtils;
 
 import java.io.*;
 import java.nio.MappedByteBuffer;
@@ -15,26 +16,27 @@ public class TableBinaryStorage implements IDatabaseReader, AutoCloseable {
     private final FileMeta _meta;
     //private final RandomAccessFile _file;
     private FileInputStream _fis = null;
-    MappedByteBuffer _inputBuffer =  null;
+    MappedByteBuffer _inputBuffer = null;
     FileChannel _inputChannel = null;
     private FileOutputStream _fos = null;
     public long readedSize;
     private long _channelSize;
-    private long METAINT = Integer.MAX_VALUE           ;
+    private long METAINT = Integer.MAX_VALUE;
 
-    public TableBinaryStorage (String table, String db, String mode) throws IOException {
+    public TableBinaryStorage(String table, String db, String mode) throws IOException {
         _table = table;
-        _db = db;
-        if (Objects.equals(mode, "rw") || Objects.equals(mode, "r")){
+        _db    = db;
+        if(Objects.equals(mode, "rw") || Objects.equals(mode, "r")) {
             _mode = mode;
-        } else {
+        }
+        else {
             _mode = "rw";
         }
         var path = Path.of(db, table + "_meta.json");
 
         BufferedReader br = new BufferedReader(new FileReader(String.valueOf(path)));
         Gson gson = new Gson();
-        _meta = gson.fromJson(br , FileMeta.class);
+        _meta = gson.fromJson(br, FileMeta.class);
         br.close();
 
 //        _file = new RandomAccessFile(Path.of(db,table).toString(), mode);
@@ -42,9 +44,10 @@ public class TableBinaryStorage implements IDatabaseReader, AutoCloseable {
             _fos = new FileOutputStream(Path.of(db, table).toString(), true);
         }
         else if(_mode.equals("r")) {
-            _fis = new FileInputStream(Path.of(db, table).toString());
+            _fis          = new FileInputStream(Path.of(db, table).toString());
             _inputChannel = _fis.getChannel();
-            _channelSize = _inputChannel.size();
+            _channelSize  = _inputChannel.size();
+//            System.err.println("GB-" + _channelSize/1024/1024/1024 + " MB-" +_channelSize/1024/1024 + " KB-" + _channelSize/1024);
             _inputBuffer = _inputChannel.map(FileChannel.MapMode.READ_ONLY, 0, CheckBufferSize());
 
         }
@@ -62,7 +65,7 @@ public class TableBinaryStorage implements IDatabaseReader, AutoCloseable {
     }
 
     public void AddData(Object[] cols) throws IOException {
-        if (!Objects.equals(_mode, "r")){
+        if(!Objects.equals(_mode, "r")) {
             TableRow row = new TableRow(cols, _meta.Types);
             row.Serialize(_fos);
         }
@@ -70,15 +73,23 @@ public class TableBinaryStorage implements IDatabaseReader, AutoCloseable {
 
     @Override
     public Object[] ReadNextRow() throws IOException, ClassNotFoundException {
-        if (_mode == "rw") return new Object[0];
+        if(Objects.equals(_mode, "rw")) return new Object[ 0 ];
+        long tmpSize = TableRow._size;
         var row = TableRow.Deserialize(_inputBuffer, _meta.Types);
-        if((_channelSize!=readedSize) && (readedSize == TableRow._size))
-        {
+        if((_channelSize != readedSize) && (TableRow._size == -1)) {
             _inputBuffer = _inputChannel.map(FileChannel.MapMode.READ_ONLY, readedSize, CheckBufferSize());
+            row          = TableRow.Deserialize(_inputBuffer, _meta.Types);
+
         }
-        assert row != null;
-        readedSize = TableRow._size;
-        return row.Columns();
+        // System.out.println("AllSize - readed = remaining " + _channelSize + "-" + readedSize + "=" + (_channelSize-readedSize) + "(" + TableRow._size + ")");
+
+        readedSize += TableRow._size;
+        if(_channelSize - readedSize > 0) {
+            assert row != null;
+            return row.Columns();
+        }
+        else
+            return new Object[]{null};
     }
 
     @Override
@@ -91,18 +102,16 @@ public class TableBinaryStorage implements IDatabaseReader, AutoCloseable {
         _inputChannel.close();
     }
 
-    private long CheckBufferSize() throws IOException {
-        System.out.println("AllSize - readed = remaining " + _channelSize + "-" + readedSize + "=" + (_channelSize-readedSize));
-        if(_channelSize <= METAINT)
-        {
+    private long CheckBufferSize(){
+        //System.out.println("AllSize - readed = remaining " + _channelSize + "-" + readedSize + "=" + (_channelSize-readedSize));
+        if(_channelSize <= METAINT) {
             return _channelSize;
         }
         else {
-            if(_channelSize-readedSize < METAINT)
-            {
-                return _channelSize-readedSize;
+            if(_channelSize - readedSize < METAINT) {
+                return _channelSize - readedSize;
             }
-            else return  METAINT;
+            else return METAINT;
         }
     }
 }
--- src/main/java/main.java
+++ src/main/java/main.java
@@ -5,85 +5,73 @@ import java.util.Date;
 
 public class main {
     public static void main(String[] args) throws Exception {
-
-//        TableRow tr = new TableRow(new Object[] {"test", 1}, new ColumnType[] {ColumnType.STRING, ColumnType.INTEGER});
-//        tr.Serialize(new FileOutputStream("test\\testing"));
-//        TableBinaryStorage tbsw = new TableBinaryStorage("testing","test","rw");
-//        tbsw.AddData(tr.Columns());
-//
-
-        //String[] tableNames = new String[]{"customer", "nation",  "part", "partsupp", "region", "supplier", "lineitem", "orders"};
-        String[] tableNames = new String[]{"lineitem"};
-        //PrintStream out = new PrintStream(new FileOutputStream("LOG.txt"));
-        //System.setErr(out);
+        String[] tableNames = new String[]{"customer", "nation", "part", "partsupp", "region", "supplier", "lineitem", "orders"};
+        //String[] tableNames = new String[]{"nation",  "part"};
         long startTime = System.currentTimeMillis();
-        //System.err.println(new Date (System.currentTimeMillis()));
+        System.err.println(new Date(System.currentTimeMillis()));
         for ( String tableName : tableNames ) {
             System.out.println(tableName);
-            TableBinaryStorage tbsr = new TableBinaryStorage(tableName,"5gb","r");
+            TableBinaryStorage tbsr = new TableBinaryStorage(tableName, "5gb", "r");
             WriteTBStoConsole(tbsr, 100000000);
             tbsr.close();
         }
         long workTime = System.currentTimeMillis() - startTime;
-        //System.out.println(workTime);
-        //System.err.println(new Date (System.currentTimeMillis()));
-        //System.err.println(workTime);
+        System.err.println(new Date(System.currentTimeMillis()));
+        System.err.println(workTime);
         //CreateBinFiles( tableNames );
-        //CreateBinFiles(  new String[] { "part"});
-
     }
 
-    private static void CreateBinFiles(String[] tableNames ) throws IOException {
-        System.out.println(new Date (System.currentTimeMillis()));
+    private static void CreateBinFiles(String[] tableNames) throws IOException {
+        System.out.println(new Date(System.currentTimeMillis()));
         for ( String tableName : tableNames ) {
-            TableBinaryStorage tbsW = new TableBinaryStorage( tableName, "test", "rw" );
-            BufferedReader reader = new BufferedReader( new FileReader( "test\\" + tableName + ".tbl" ) );
-            String line = reader.readLine( );
-            System.out.println(tableName+ "........Started!");
+            TableBinaryStorage tbsW = new TableBinaryStorage(tableName, "test", "rw");
+            BufferedReader reader = new BufferedReader(new FileReader("test\\" + tableName + ".tbl"));
+            String line = reader.readLine();
+            System.out.println(tableName + "........Started!");
             while (line != null) {
-                var parts = line.split( "\\|" );
-                var row = new Object[tbsW.Meta().Columns.length];
+                var parts = line.split("\\|");
+                var row = new Object[ tbsW.Meta().Columns.length ];
                 for ( var i = 0; i < tbsW.Meta().Columns.length; i++ ) {
-                    switch (tbsW.Meta().Types[i])
-                    {
-                        case LONG -> row[i] = Long.parseLong(parts[i]);
-                        case INTEGER -> row[i] = Integer.parseInt(parts[i]);
-                        case FLOAT -> row[i] = Float.parseFloat(parts[i]);
-                        case DOUBLE -> row[i] = Double.parseDouble(parts[i]);
-                        case BOOLEAN -> row[i] = Boolean.parseBoolean(parts[i]);
-                        case STRING -> row[i] = parts[i];
+                    switch (tbsW.Meta().Types[ i ]) {
+                        case LONG -> row[ i ] = Long.parseLong(parts[ i ]);
+                        case INTEGER -> row[ i ] = Integer.parseInt(parts[ i ]);
+                        case FLOAT -> row[ i ] = Float.parseFloat(parts[ i ]);
+                        case DOUBLE -> row[ i ] = Double.parseDouble(parts[ i ]);
+                        case BOOLEAN -> row[ i ] = Boolean.parseBoolean(parts[ i ]);
+                        case STRING -> row[ i ] = parts[ i ];
                         case DATE, TIME, DATETIME -> {
 
-                            SimpleDateFormat f = new SimpleDateFormat ( "yyyy-MM-DD" );
+                            SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd");
                             try {
-                                Date d = f.parse( parts[i] );
-                                row[i] = d.getTime ();
+                                Date d = f.parse(parts[ i ]);
+                                row[ i ] = d.getTime();
                             } catch (ParseException e) {
-                                e.printStackTrace ( );
+                                e.printStackTrace();
                             }
                         }
-                        default -> {}
+                        default -> {
+                        }
                     }
                 }
-                tbsW.AddData( row );
-                line = reader.readLine( );
+                tbsW.AddData(row);
+                line = reader.readLine();
             }
-            reader.close( );
+            reader.close();
             System.out.println(tableName + "........Done!");
         }
-        System.out.println(new Date (System.currentTimeMillis()));
+        System.out.println(new Date(System.currentTimeMillis()));
     }
 
     private static void WriteTBStoConsole(TableBinaryStorage tbsr, int linesCount) throws IOException, ClassNotFoundException {
         var data = tbsr.ReadNextRow();
         int j = 0;
         while (j < linesCount && data[ 0 ] != null) {
-//            for ( int i = 0; i < data.length; i++ ) {
-//                System.out.print(data[ i ]);
-//                if(i != data.length - 1) System.out.print("|");
-//            }
+            for ( int i = 0; i < data.length; i++ ) {
+                System.out.print(data[ i ]);
+                if(i != data.length - 1) System.out.print("|");
+            }
             j++;
-//            System.out.println();
+            System.out.println();
             data = tbsr.ReadNextRow();
 
         }
